2dd9355274e15319bb8c7af7fe351deb3d25a8cd
biamichelle <biamichelle@usp.br>
2018-01-26 15:21:07 -0200

    Modidy class Cow, add methods and modify class app

--- myFarm/src/main/java/com/thoughtworks/App.java
+++ myFarm/src/main/java/com/thoughtworks/App.java
@@ -5,13 +5,21 @@ public class App
 {
     public static void main( String[] args ) {
 
+        Cow cow1 = new Cow();
+        cow1.name = "Emma";
+        cow1.age = 5;
+        cow1.calculateTime(40);
+        System.out.println(cow1.printInformationCow());
+
+        Cow cow2 = new Cow();
+        cow2.name = "Penelope";
+        cow2.age = 8;
+        cow2.calculateTime(80);
+        System.out.println(cow2.printInformationCow());
+
         Dog dog = new Dog();
         System.out.println(dog.greeting());
         System.out.println(dog.run());
-
-        Cow cow = new Cow();
-        System.out.println(cow.greeting());
-        System.out.println(cow.run());
 
         Pig pig = new Pig();
         System.out.println(pig.greeting());
--- myFarm/src/main/java/com/thoughtworks/Cow.java
+++ myFarm/src/main/java/com/thoughtworks/Cow.java
@@ -2,11 +2,28 @@ package com.thoughtworks;
 
 public class Cow extends Animal {
 
+    String name;
+    int age;
+    int time;
+
+
     public String greeting() {
         return "Mooom";
     }
 
     public String run() {
         return "Speed is 30km/h";
+    }
+
+    public void calculateTime(int distance){
+        time = distance / 30;
+    }
+
+    public String printInformationCow(){
+        String dados = "\n Name:" + this.name;
+        dados += "\n Idade:" + this.age +"years old";
+        dados += "\n Time is: " + this.time + " Km/h";
+        dados += "\n" + this.greeting();
+        return dados;
     }
 }
